b63be3b9315a25a896700b3a219e9f29e44931d2
Oleg <oleg.yevik@hapara.com>
2020-05-08 17:36:24 +1200

    Styling

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,71 +1,70 @@
 import 'package:flutter/material.dart';
 import 'sharedFuncs/funcs.dart';
 
-class HomeScreen extends StatefulWidget {
-  HomeScreen({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-        width: double.infinity,
-        height: 150.0,
-        child: Column(
-          children: <Widget>[
-            Row(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+            width: double.infinity,
+            height: 150.0,
+            child: Column(
               children: <Widget>[
-                Text(
-                  'Hello,  How are you?',
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 34.0,
-                    fontFamily: 'Lato',
-//                    fontFamily:
-//                      font-family: SF UI Display;
-//                      font-size: 34px;
-//                  line-height: 41px;
-                  ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'Hello,  How are you?',
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 34.0,
+                        fontFamily: 'Lato',
+                      ),
+                    ),
+                    Text('asdf')
+                  ],
                 ),
-                Text('asdf')
               ],
             ),
-          ],
-        ),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              hexToColor('#599BF0'),
-              hexToColor('#4785EB'),
-            ],
-            begin: Alignment.bottomLeft,
-            end: Alignment.topRight,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  hexToColor('#599BF0'),
+                  hexToColor('#4785EB'),
+                ],
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+              ),
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40)),
+            ),
           ),
-          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40)),
-        ),
+          Container(
+            child: Text('asFsdsad'),
+          ),
+          Container(
+            child: Text('asFsdsad'),
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  width: 3.0,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            padding: const EdgeInsets.symmetric(
+              vertical: 15.0,
+              horizontal: 20.0,
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
