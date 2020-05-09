23861730355364ec027a67624af74551248f3601
Oleg <oleg@vesta-central.com>
2020-05-09 14:45:09 +1200

    Footer

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -49,19 +49,70 @@ class HomeScreen extends StatelessWidget {
             child: Text('asFsdsad'),
           ),
           Container(
-            child: Text('asFsdsad'),
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(
-                  width: 3.0,
-                  color: Colors.black,
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.volume_up),
+                  tooltip: 'Increase volume by 10',
+                  onPressed: () {
+                    setState(() {
+                      _volume += 10;
+                    });
+                  },
                 ),
+                (
+                  Icons.home,
+                  color: Colors.grey,
+                  size: 24.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                  label: '',
+                ),
+              ],
+            ),
+            height: 97.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
               ),
+              boxShadow: [
+                BoxShadow(
+                  color: hexToColor("#999999").withOpacity(0.16),
+                  spreadRadius: 5,
+                  blurRadius: 15,
+                  offset: Offset(0, -3), // changes position of shadow
+                ),
+              ],
             ),
-            padding: const EdgeInsets.symmetric(
-              vertical: 15.0,
-              horizontal: 20.0,
-            ),
+//            color: Colors.red,
+//            decoration: BoxDecoration(
+//              border: Border(
+//                top: BorderSide(
+//                  width: 3.0,
+//                  color: Colors.black,
+//                ),
+//              ),
+//              borderRadius: BorderRadius.only(
+//                topLeft: Radius.circular(10),
+//                topRight: Radius.circular(10),
+//                bottomLeft: Radius.circular(10),
+//                bottomRight: Radius.circular(10),
+//              ),
+////              boxShadow: [
+////                BoxShadow(
+////                  color: Colors.grey.withOpacity(0.5),
+////                  spreadRadius: 5,
+////                  blurRadius: 7,
+////                  offset: Offset(0, 3), // changes position of shadow
+////                ),
+////              ],
+//            ),
+//            padding: const EdgeInsets.symmetric(
+//              vertical: 15.0,
+//              horizontal: 20.0,
+//            ),
           ),
         ],
       ),
